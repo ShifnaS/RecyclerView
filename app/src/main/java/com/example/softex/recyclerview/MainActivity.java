1b00f2f93203c0a2224527782edcb4812bce8f23
ShifnaS <42054421+ShifnaS@users.noreply.github.com>
2018-08-09 11:32:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/softex/recyclerview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.softex.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Helloo Welcome", Toast.LENGTH_SHORT).show();
+    }
+}
